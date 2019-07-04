920ab205b9ba1b52471f3cabc0ca797f174c3288
IkezawaYuki <y.ikezawa93@gmail.com>
2019-07-04 22:49:03 +0900

    add api folder

---
+++ api/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"gopkg.in/mgo.v2"
+	"net/http"
+)
+
+func main(){
+
+}
+
+
+func WithAPIKey(fn http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request){
+		if !isValidAPIKey(r.URL.Query().Get("key")){
+			respondErr(w, r, http.StatusUnauthorized, "不正なAPIキーです")
+			return
+		}
+		fn(w, r)
+	}
+}
+
+func isValidAPIKey(key string)bool{
+	return key == "abc123"
+}
+
+func withData(d *mgo.Session, f http.HandlerFunc) http.HandlerFunc  {
+	return func(w http.ResponseWriter, r *http.Request) {
+		thisDb := d.Copy()
+		defer thisDb.Close()
+		SetVar(r, "db", thisDb.DB("ballots"))
+		f(w, r)
+	}
+}
+
+func withVars(fn http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter, r *http.Request) {
+		OpenVars(r)
+		defer CloseVars(r)
+		fn(w, r)
+	}
+}
