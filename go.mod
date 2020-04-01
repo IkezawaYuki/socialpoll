d15a97658a66f85d57903e037afec0acfc8bb633
yikezawa <y.ikezawa93@gmail.com>
2020-04-01 22:54:53 +0900

    mod

--- go.mod
+++ go.mod
@@ -5,5 +5,8 @@ go 1.14
 require (
 	github.com/garyburd/go-oauth v0.0.0-20180319155456-bca2e7f09a17 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- twittervotes/main.go
+++ twittervotes/main.go
@@ -1,5 +1,38 @@
 package main
 
+import (
+	"gopkg.in/mgo.v2"
+	"log"
+)
+
 func main()  {
 
 }
+
+var db *mgo.Session
+func dialdb() error{
+	var err error
+	log.Println("MongoDBにダイヤル中: localhost")
+	db, err = mgo.Dial("localhost")
+	return err
+}
+
+func closedb(){
+	db.Close()
+	log.Println("データベースの接続が閉じられました")
+}
+
+type poll struct {
+	Options []string
+}
+func loadOptions()([]string, error){
+	var options []string
+	iter := db.DB("ballots").C("polls").Find(nil).Iter()
+	var p poll
+	for iter.Next(&p){
+		options = append(options, p.Options...)
+	}
+	iter.Close()
+	return options, iter.Err()
+}
+
